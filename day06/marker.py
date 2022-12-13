129a3766002f3deef9301ad76dcd583fabaf9f9e
albinwk <albinwk@kth.se>
2022-12-13 23:15:38 +0100

    day 6

--- day06/marker.py
+++ day06/marker.py
@@ -1,11 +1,12 @@
 input = open("input.txt", "r")
 data = input.readlines()[0]
 last_chars = [0 for _ in range(14)]
+d = 14
 
 i = 0
 for c in data:
-    last_chars[i % 14] = c
-    if len(last_chars) <= len(set(last_chars)) and i > 13:
+    last_chars[i % d] = c
+    if len(last_chars) <= len(set(last_chars)) and i >= d:
         break
     i += 1
 
